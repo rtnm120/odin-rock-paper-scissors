4cedcebe089cf146d4a1603e2c31925b8de7ef59
rtnm120 <37772451+rtnm120@users.noreply.github.com>
2024-03-16 20:57:48 +1100

    Implement gui and game logic

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -6,55 +6,149 @@ function getComputerChoice() {
   return computerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
   playerSelection = playerSelection.toLowerCase();
+  const computerSelection = getComputerChoice();
 
-  let results =
-    playerSelection == computerSelection
-      ? "It's a tie!"
-      : playerSelection == "rock" && computerSelection == "paper"
-        ? "You Lose! Paper beats Rock"
-        : playerSelection == "rock" && computerSelection == "scissors"
-          ? "You Win! Rock beats Scissors"
-          : playerSelection == "paper" && computerSelection == "rock"
-            ? "You Win! Rock beats Scissors"
-            : playerSelection == "paper" && computerSelection == "scissors"
-              ? "You Lose! Scissors beats Paper"
-              : playerSelection == "scissors" && computerSelection == "paper"
-                ? "You Win! Scissors beats Paper"
-                : playerSelection == "scissors" && computerSelection == "rock"
-                  ? "You Lose! Rock beats Scissors"
-                  : "Please enter Rock, Paper or Scissors";
-  return results;
-}
-
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (i = 0; i < 5; i++) {
-    let playerSelection = prompt("Rock, Paper or Scissors?\n");
-
-    let results = playRound(playerSelection, getComputerChoice());
-
-    if (results.slice(4, 5) == " ") {
-      playerScore++;
-      computerScore++;
-    } else if (results.slice(4, 5) == "W") {
-      playerScore++;
-    } else {
-      computerScore++;
-    }
-
-    console.log(`Round ${i + 1}: ${results}`);
+  if (playerScore == 5 || computerScore == 5) {
+    return;
   }
 
-  let results =
-    playerScore == computerScore
-      ? `Both you and the computer scored ${playerScore} points. It's a tie!`
-      : playerScore > computerScore
-        ? `You win with ${playerScore} points!`
-        : `You lose! The computer scored ${computerScore} points`;
+  let results = "";
 
-  return results;
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    results = "w";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")
+  ) {
+    results = "l";
+  } else {
+    results = "t";
+  }
+
+  updateScore(results, playerSelection, computerSelection);
 }
+
+function updateScore(results, playerSelection, computerSelection) {
+  const playerScoreboard = document.querySelector(".player-score h1");
+  const computerScoreboard = document.querySelector(".computer-score h1");
+  const playerImage = document.querySelector(".player-image");
+  const computerImage = document.querySelector(".computer-image");
+  const roundDisplay = document.querySelector(".round-display");
+  const roundSummary = document.querySelector(".round-summary");
+  const replayChildren = document.querySelectorAll(".replay *");
+
+  let resultMessage = "";
+  let summaryMessage = "";
+
+  if (replayChildren[0].classList.contains("hidden")) {
+    replayChildren.forEach((child) => {
+      child.classList.remove("hidden");
+    });
+  }
+
+  playerImage.setAttribute("src", `images/${playerSelection}.png`);
+  computerImage.setAttribute("src", `images/${computerSelection}.png`);
+
+  if (results == "w") {
+    playerScore++;
+    resultMessage = "You Win!";
+    summaryMessage = `${toTitleCase(playerSelection)} beats ${toTitleCase(computerSelection)}`;
+  } else if (results == "l") {
+    computerScore++;
+    resultMessage = "You Lose!";
+    summaryMessage = `${toTitleCase(computerSelection)} beats ${toTitleCase(playerSelection)}`;
+  } else {
+    playerScore++;
+    computerScore++;
+    resultMessage = "It's a Tie!";
+    summaryMessage = `Both players picked ${toTitleCase(playerSelection)}`;
+  }
+
+  roundDisplay.textContent = resultMessage;
+  roundSummary.textContent = summaryMessage;
+  playerScoreboard.textContent = playerScore;
+  computerScoreboard.textContent = computerScore;
+
+  if (playerScore == 5 || computerScore == 5) {
+    displayWinner();
+  }
+}
+
+function displayWinner() {
+  const container = document.querySelector("body div");
+  const display = document.createElement("div");
+  const message = document.createElement("h3");
+  const resetButton = document.createElement("button");
+
+  //display.classList.add("flex-container", "flex-column");
+
+  if (playerScore > computerScore) {
+    message.textContent = "You were the first to 5 points!";
+  } else if (computerScore > playerScore) {
+    message.textContent = "The Computer was the first to 5 points!";
+  } else {
+    message.textContent = "It's a tie! Both players have reached 5 points!";
+  }
+
+  resetButton.textContent = "Play Again?";
+
+  display.appendChild(message);
+  display.appendChild(resetButton);
+  container.appendChild(display);
+
+  resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+  const replayChildren = document.querySelectorAll(".replay *");
+  const playerScoreboard = document.querySelector(".player-score h1");
+  const computerScoreboard = document.querySelector(".computer-score h1");
+  const roundDisplay = document.querySelector(".round-display");
+  const roundSummary = document.querySelector(".round-summary");
+
+  replayChildren.forEach((child) => {
+    child.classList.add("hidden");
+  });
+
+  roundDisplay.textContent = "";
+  roundSummary.textContent = "";
+
+  playerScore = 0;
+  computerScore = 0;
+
+  playerScoreboard.textContent = playerScore;
+  computerScoreboard.textContent = computerScore;
+
+  document.querySelector("button").parentElement.remove();
+}
+
+function toTitleCase(string) {
+  return `${string.slice(0, 1).toUpperCase()}${string.slice(1)}`;
+}
+
+const playerChoices = document.querySelectorAll(".player-selection img");
+const rock = playerChoices[0];
+const paper = playerChoices[1];
+const scissors = playerChoices[2];
+
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener("click", () => {
+  playRound("rock");
+});
+
+paper.addEventListener("click", () => {
+  playRound("paper");
+});
+
+scissors.addEventListener("click", () => {
+  playRound("scissors");
+});
